d9690271fb66b6c28b2c40606e73db22268383e1
DonaldFromForsythe <171101094@live.unilag.edu.ng>
2025-12-27 21:53:02 +0100

    Update auto_timestamp.ts

--- scripts/auto_timestamp.ts
+++ scripts/auto_timestamp.ts
@@ -1,1 +1,15 @@
 
+function main(workbook: ExcelScript.Workbook) {
+   //Select the active sheet
+  const sheet: ExcelScript.Worksheet = workbook.getActiveWorksheet();
+
+/*On the workbook there are 4 mains Sheets for each quarter to prevent overloading
+Select the table that starts with "Q" (for Quarter) in the active sheet*/
+  
+  const table: ExcelScript.Table = sheet.getTables().find(t => t.getName().startsWith("Q"));
+  if (!table) {
+    console.log("Table not found.");
+    return;
+  }
+
+}
